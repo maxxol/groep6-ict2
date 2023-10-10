7096f03aa1a0bd789fc5a5c9674dcd939c4f119c
Jeroen Lüling <jtube2014@gmail.com>
2023-10-10 11:43:35 +0200

    ADD: Initial Theme Park test

---
+++ controls.js
@@ -0,0 +1,168 @@
+import * as THREE from "three";
+import {camera} from "./main"
+
+const player = new THREE.Object3D();
+
+function loadplayer(scene, camera) {
+    // Add the passed camera to the player object
+    const player = new THREE.Object3D();
+    player.add(camera);
+    camera.eulerOrder = "YXZ";
+
+    // Set the initial camera position and rotation
+    camera.rotation.set(0, 1.5*Math.PI, 0);
+    camera.position.set(0, 0.6, 0);
+
+
+    // Set the initial player position
+    player.position.set(0, 0, 0);
+
+    // Add the player object to the passed scene
+    scene.add(player);
+
+    //console.log("(controls.js)Player loaded in at " + player.position.x + " " + player.position.z); //debug
+}
+
+function getPlayer() {
+    return player;
+}
+
+export { loadplayer, getPlayer };
+
+// creating control booleans, default on false
+const controls = {
+    moveForward: false,
+    moveBackward: false,
+    moveLeft: false,
+    moveRight: false,
+    moveUp: false,
+    moveDown: false,
+    canJump: false,
+    prevTime: performance.now(),
+    velocity: new THREE.Vector3(),
+};
+
+
+document.addEventListener('keydown', (event) => { //listeners for key presses
+    switch (event.code) {
+        case 'KeyW':
+            controls.moveForward = true;
+            break;
+        case 'KeyA':
+            controls.moveLeft = true;
+            break;
+        case 'KeyS':
+            controls.moveBackward = true;
+            break;
+        case 'KeyD':
+            controls.moveRight = true;
+            break;
+        case 'Space':
+            if (controls.canJump === true) controls.velocity.y += 40;
+            controls.canJump = false;
+            break;
+        case 'ShiftLeft':
+            controls.moveDown = true;
+            break;
+    }
+});
+
+document.addEventListener('keyup', (event) => { //listeners for keys being unpressed
+    switch (event.code) {
+        case 'KeyW':
+            controls.moveForward = false;
+            break;
+        case 'KeyA':
+            controls.moveLeft = false;
+            break;
+        case 'KeyS':
+            controls.moveBackward = false;
+            break;
+        case 'KeyD':
+            controls.moveRight = false;
+            break;
+        case 'ShiftLeft':
+            controls.moveDown = false;
+            break;
+    }
+});
+
+//variables for mouse movement
+const mouse = new THREE.Vector2();
+const sensitivityX = 0.003;
+const sensitivityY = 0.003;
+const maxPitch = Math.PI / 2 - 0.01;
+const minPitch = -maxPitch;
+
+document.addEventListener('mousemove', (event) => {
+    camera.rotation.order = "YXZ"; // Set the camera's eulerOrder to "YXZ" (in listener because of "not initialised" shenanigans)
+
+    camera.rotation.y -= event.movementX * sensitivityX; // Rotate the camera around the y-axis (yaw)
+
+    const pitchRotation = event.movementY * sensitivityY;// Calculate the pitch rotation (rotation around x-axis)
+    camera.rotation.x -= pitchRotation;// Apply the pitch rotation to the camera
+    camera.rotation.x = Math.max(minPitch, Math.min(maxPitch, camera.rotation.x)); // Limit the pitch rotation (no further than straight up or down)
+
+    camera.updateProjectionMatrix(); // Update the camera's rotation
+});
+
+
+//animate controls function
+export function updatecontrols(camera,time,delta) {
+    const speed = 30; // Adjust this value for movement speed
+    const gravity = 10; // Adjust this value for gravity strength
+
+    const cameraRotation = camera.getWorldQuaternion(new THREE.Quaternion());
+
+// Calculate the camera's forward direction vector
+    const direction = new THREE.Vector3(0, 0, -1);
+    direction.applyQuaternion(cameraRotation);
+
+// Handle movement based on control inputs
+//     if (controls.moveForward) {
+//         player.position.add(direction.clone().multiplyScalar(speed * delta));
+//     }
+    if (controls.moveForward) {
+        const forwardDirection = new THREE.Vector3(direction.x, 0, direction.z); // Create a direction vector that ignores the Y component
+        forwardDirection.normalize();// Normalize the forwardDirection vector
+        player.position.add(forwardDirection.clone().multiplyScalar(speed * delta)); // Move the player along the modified forwardDirection vector
+    }
+    if (controls.moveBackward) {
+        const backwardDirection = new THREE.Vector3(direction.x, 0, direction.z); //same but for backward
+        backwardDirection.normalize();
+        player.position.add(backwardDirection.clone().multiplyScalar(-speed * delta)); //-speed because backward = -forward
+    }
+
+    if (controls.moveLeft) {
+        const leftDirection = new THREE.Vector3(-1, 0, 0); // Create a direction vector that represents right movement (-x is right when camerarotation = 0,0,0)
+        leftDirection.applyQuaternion(cameraRotation); // Apply the camera's rotation to the left Direction vector
+        leftDirection.normalize();// Normalize the leftDirection vector
+        player.position.add(leftDirection.clone().multiplyScalar(speed * delta)); // Move the player right along the modified leftDirection vector
+    }
+
+    if (controls.moveRight) {
+        const rightDirection = new THREE.Vector3(1, 0, 0); // same but for right
+        rightDirection.applyQuaternion(cameraRotation);
+        rightDirection.normalize();
+        player.position.add(rightDirection.clone().multiplyScalar(speed * delta));
+    }
+
+
+// Update the player's position with gravity
+    controls.velocity.y -= gravity * 9.8 * delta;
+    player.position.add(controls.velocity.clone().multiplyScalar(delta));
+// Collision detection (you can add your own collision logic here)
+
+// Prevent the camera from falling through the ground
+    if (player.position.y < 1.6) {
+        controls.velocity.y = 0;
+        player.position.y = 1.6;
+        controls.canJump = true;
+    }
+
+    controls.prevTime = time;
+
+
+}
+
+export default controls
---
+++ main.js
@@ -0,0 +1,272 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import controls, {loadplayer, getPlayer,updatecontrols } from "./controls"
+;
+//import {Reflector} from 'three/examples/jsm/objects/Reflector'
+//------------------------------------------------------------------------------------------------------------------------------------
+
+//scene
+const scene = new THREE.Scene();
+
+//camera
+const camera = new THREE.PerspectiveCamera(75,16/9,0.1,20000);
+camera.position.set(1, 1, 6);
+
+export {camera,scene}; //export for controls.js
+
+//renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth,window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+loadplayer(scene,camera); //loads player from controls.js
+const player = getPlayer();
+//console.log("(main)player loaded in at "+player.position.x+" "+player.position.z) //debug
+
+//---------------------------------------------------------------------------------------------------------------
+//lighting
+
+// const lightA = new THREE.AmbientLight( 0xFFFFFF,10 ); // soft white ambient light for debug
+// scene.add( lightA );
+
+//directional light to simulate sun
+const lightD = new THREE.DirectionalLight(0xA0A0A0, 1)
+scene.add(lightD)
+lightD.position.set(0,1,0)
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------------
+//environment
+//skybox sphere
+// Load the equirectangular (panorama) texture
+const loadersphere = new THREE.TextureLoader();
+const skyboxTexture = loadersphere.load('assets/textures/skybox/skyboxspace.png');
+
+// Create a sphere geometry for the skybox
+const geometrySky = new THREE.SphereGeometry(10000, 32, 32); // Adjust the radius as needed
+const materialSky = new THREE.MeshBasicMaterial({ map: skyboxTexture, side: THREE.BackSide }); // Define the material for the skybox
+const skybox = new THREE.Mesh(geometrySky, materialSky); // Create the skybox mesh
+scene.add(skybox); // Add the skybox to your scene
+
+//ground
+const loaderground = new THREE.TextureLoader();
+const groundtexture = loaderground.load('assets/textures/ground/groundtext.jpg');
+
+groundtexture.wrapS = THREE.RepeatWrapping; //wraps x axis
+groundtexture.wrapT = THREE.RepeatWrapping; //wraps z axis
+groundtexture.repeat.set(100, 100); // amount of tiles per axis x,z
+
+const geometryground = new THREE.BoxGeometry(1000, 1, 1000);
+const materialground = new THREE.MeshLambertMaterial({ map: groundtexture });
+const ground = new THREE.Mesh(geometryground, materialground);
+
+scene.add(ground);
+ground.scale.set(1,1,1);
+ground.position.set(0,0,0);
+
+//-----------------------------------------------------------------------------------------------------------------
+//lightposts
+class LightPost {
+    constructor(scene) {
+        this.scene = scene;
+    }
+
+    create(x, z) {
+        // Post without the 'glass' containing the lamp
+        const geometryPost = new THREE.BoxGeometry(0.7, 5, 0.7);
+        const materialPost = new THREE.MeshPhongMaterial({ color: 0xCCCCCC });
+        this.lampPost = new THREE.Mesh(geometryPost, materialPost);
+        this.lampPost.position.set(x, 3, z);
+        this.scene.add(this.lampPost);
+
+        // Glass containing the lamp
+        const geometryPostGlass = new THREE.BoxGeometry(1, 1, 1);
+        const materialPostGlass = new THREE.MeshBasicMaterial({ color: 0xFFFF00 });
+        this.lampPostGlass = new THREE.Mesh(geometryPostGlass, materialPostGlass);
+        this.lampPostGlass.position.set(x, 5.8, z);
+        this.scene.add(this.lampPostGlass);
+
+        // Light emitter
+        this.lightPostLamp = new THREE.PointLight(0xFFFFFF, 100);
+        this.lightPostLamp.position.set(x, 5.8, z);
+        this.scene.add(this.lightPostLamp);
+    }
+}
+
+// Create light posts
+const lightPosts = new LightPost(scene);
+
+// Lights at opening path
+lightPosts.create(10, 4);
+lightPosts.create(10, -10);
+
+// Light at spinning cubes
+lightPosts.create(40, 4);
+lightPosts.create(40, -10);
+
+// Lights at skull
+lightPosts.create(40, 47);
+lightPosts.create(54, 47);
+lightPosts.create(54, 33);
+
+//lights at coaster test
+lightPosts.create(90,47);
+lightPosts.create(90,33);
+
+//----------------------------------------------------------------------------------------------------------------
+//spinning cube
+
+const geometryspin = new THREE.BoxGeometry( 2, 2, 2 );
+const materialspin = new THREE.MeshBasicMaterial( { color: 0xF0ff00 } );
+const spinningcube1 = new THREE.Mesh(geometryspin, materialspin);
+scene.add( spinningcube1 );
+spinningcube1.position.set(55,5,-3)
+
+const geometryspin2 = new THREE.BoxGeometry( 1, 1, 1 );
+const materialspin2 = new THREE.MeshPhongMaterial( { color: 0xFF8080 } );
+const spinningcube2 = new THREE.Mesh(geometryspin2, materialspin2);
+scene.add( spinningcube2 );
+spinningcube2.position.set(55,5,-2.8)
+
+const geometryspin3 = new THREE.BoxGeometry( 1, 1, 1 );
+const materialspin3 = new THREE.MeshPhongMaterial( { color: 0x00FFFF } );
+const spinningcube3 = new THREE.Mesh(geometryspin3, materialspin3);
+scene.add( spinningcube3 );
+spinningcube3.position.set(55,5,-2.8)
+
+const lightP = new THREE.PointLight( 0xFFFFFF, 100  );
+lightP.position.set( spinningcube1.position.x,spinningcube1.position.y,spinningcube1.position.z );
+scene.add( lightP );
+
+//variables for cube movement and rotation
+const radius = 5; // Adjust the radius of the circular motion
+const angularSpeed = 0.004; // Adjust the speed of rotation
+//cube movement and rotation function
+function spinthecubes(time) {
+    // Calculate the current angle based on time and angular speed
+    const angle = time * angularSpeed;
+
+    // Calculate the positions of spinningcube2 and spinningcube3
+    const cube2X = spinningcube1.position.x + radius * Math.cos(angle); //center of procesion(spinningcube)
+    const cube2Z = spinningcube1.position.z + radius * Math.sin(angle);
+
+    const cube3X = spinningcube1.position.x + radius * Math.cos(angle + Math.PI); // 180 degrees out of phase
+    const cube3Z = spinningcube1.position.z + radius * Math.sin(angle + Math.PI); // 180 degrees out of phase
+
+    // cube2 and cube3 rotate
+    spinningcube2.position.set(cube2X, spinningcube2.position.y, cube2Z);
+    spinningcube3.position.set(cube3X, spinningcube3.position.y, cube3Z);
+
+    spinningcube1.rotation.x += 0.09;
+    spinningcube1.rotation.y += 0.09;
+
+    spinningcube2.rotation.x -= 0.09;
+    spinningcube2.rotation.y -= 0.09;
+
+    spinningcube3.rotation.x -= 0.09;
+    spinningcube3.rotation.y -= 0.09;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------
+//pathways
+function addPath(x,z,width,depth){
+    const geometry = new THREE.BoxGeometry( width, 1, depth );
+    const material = new THREE.MeshPhongMaterial( { color: 0xF0F0F0 } );
+    const path = new THREE.Mesh(geometry, material);
+    scene.add(path);
+    path.position.set(x,0.1,z)
+}
+addPath(25,-3,50,6)
+addPath(47,25,6,50)
+addPath(70,40,50,6)
+addPath(115,40,40,40)
+
+//------------------------------------------------------------------------------------------------------------------------------------
+//3d models
+
+const loader = new GLTFLoader();//loader
+
+let modelglobal; //global variable for the skull model, so it can be called in the animate function
+async function loadModels() {
+    try {
+        const skullModelFile = await loader.loadAsync('assets/3d_models/human_skull_free.glb'); //loads skull model
+        const model = skullModelFile.scene;
+        scene.add(model); //adds skull to the scene
+        //model.scale.set(2,2,2); //make skull bigger
+        model.position.set(47,6,54)
+        modelglobal=model; //makes it possible for animate function to call the model
+
+    } catch (error) {
+        console.error('An error occurred while loading the model:', error); //oh shit oh fuck
+    }
+}
+loadModels() //load the skull model
+//--------------------------------------------------------------------------------------------------------------
+//mirror test
+// const mirror:Reflector = new Reflector(
+//     new THREE.PlaneGeometry(2,2),{
+//         color:new THREE.color(0xFFFFFF),
+//         textureWidth:window.innerWidth*window.devicePixelRatio,
+//         textureHeight:window.innerHeight*window.devicePixelRatio,
+//     }
+// )
+//----------------------------------------------------------------------------------------------------------------
+//coaster test
+const geometryCoasterCart = new THREE.BoxGeometry( 2, 2, 2 );
+const materialCoasterCart = new THREE.MeshPhongMaterial( { color: 0xFF8080 } );
+const CoasterCart = new THREE.Mesh(geometryCoasterCart, materialCoasterCart);
+scene.add( CoasterCart );
+CoasterCart.position.set(115,3,40)
+
+const radiusCoaster = 15; // Adjust the radius of the circular motion
+const angularSpeedCoaster = 0.004; // Adjust the speed of rotation
+function moveCoaster(time) {
+    // Calculate the current angle based on time and angular speed
+    const angle = time * angularSpeedCoaster;
+
+    // Calculate the positions of spinningcube2 and spinningcube3
+    const coasterCartX = 115 + radiusCoaster * Math.cos(angle); //center of procesion(spinningcube)
+    const coasterCartZ = 40 + radiusCoaster * Math.sin(angle);
+    CoasterCart.lookAt(115,3,40)
+    CoasterCart.position.set(coasterCartX, 4, coasterCartZ);
+}
+//----------------------------------------------------------------------------------------------------------------
+
+console.log("(main pre animate)player loaded in at "+player.position.x+" "+player.position.z)
+// Animation loop
+const animate = () => {
+    requestAnimationFrame(animate); //rendering a frame
+    renderer.render(scene, camera);
+
+    const time = performance.now();
+    const delta = (time - controls.prevTime) / 1000;
+
+
+    updatecontrols(camera,time,delta);
+    camera.position.set(player.position.x,player.position.y+4.6,player.position.z)
+
+    // Rotate the majestic cube of death
+    spinthecubes(time);
+    moveCoaster(time);
+
+    if(player.position.x >92 && player.position.x<130 && player.position.z>20 && player.position.z<60){
+        camera.position.set(CoasterCart.position.x,CoasterCart.position.y+2,CoasterCart.position.z)
+
+        camera.lookAt(115,5,40)
+        camera.rotation.y += 90;
+    }
+
+
+    modelglobal.rotation.y += 0.04; //rotate skull model
+
+    renderer.setSize(window.innerWidth,window.innerHeight); //changes main.js module to fit in window every frame
+
+
+    //console.log(camera.rotation); //camera rotation debug
+    console.log("(main in animate)player loaded in at "+player.position.x+" "+player.position.z)    //debug
+    //console.log("(main in animate)camera rotation "+camera.rotation.x+" "+camera.rotation.y+" "+camera.rotation.z)    //debug
+
+};
+
+animate();
