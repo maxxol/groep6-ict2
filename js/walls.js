eb5d7354ed12f92f929933044d3c5214eeb5d327
Jeroen Lüling <jtube2014@gmail.com>
2023-10-13 16:45:06 +0200

    added more wall objects, play area is now encapsulated in walls

--- js/walls.js
+++ js/walls.js
@@ -17,9 +17,14 @@ class Wall{
     }
 }
 
+//function to load all the walls, called from main
 function buildWalls(scene){
     const wall1 = new Wall(scene,180,0,-10,0)
-    const wall2 = new Wall(scene,140,0.5*Math.PI,60,90)
+    const wall2 = new Wall(scene,160,0.5*Math.PI,70,90) //0.5*Math.PI means the wall is rotated 90 degrees
+    const wall3 = new Wall(scene,80,0,150,50)
+    const wall4 = new Wall(scene,80,0.5*Math.PI,110,10)
+    const wall5 = new Wall(scene,40,0,70,0)
+    const wall6 = new Wall(scene,100,0.5*Math.PI,40,-20)
 }
 
 export {buildWalls};
