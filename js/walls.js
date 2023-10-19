be909c81bf132354cb16e9e7cb122092e361542e
Jeroen Lüling <jtube2014@gmail.com>
2023-10-19 18:56:37 +0200

    added an extra path to a new section of the park, added and changed some walls

--- js/walls.js
+++ js/walls.js
@@ -19,12 +19,17 @@ class Wall{
 
 //function to load all the walls, called from main.
 function buildWalls(scene){
-    const wall1 = new Wall(scene,180,0,-10,0)
-    const wall2 = new Wall(scene,160,0.5*Math.PI,70,90) //0.5*Math.PI means the wall is rotated 90 degrees
-    const wall3 = new Wall(scene,80,0,150,50)
-    const wall4 = new Wall(scene,80,0.5*Math.PI,110,10)
-    const wall5 = new Wall(scene,40,0,70,0)
-    const wall6 = new Wall(scene,100,0.5*Math.PI,40,-20)
+    //notes:
+    //0.5*Math.PI means the wall is rotated 90 degrees.
+    //0 means it would be perpendicular to player spawn rotation, 90 means parallel to player spawn rotation
+    //there are no walls that are in the same plane and touching, meaning any continuous looking wall is a continuous wall.
+    const wall1 = new Wall(scene,180,0,-10,0) //wall behind player spawn
+    const wall2 = new Wall(scene,160,0.5*Math.PI,70,90)  //long wall to the right of player spawn
+    const wall3 = new Wall(scene,200,0,150,-10) // wall behind carousel
+    const wall4 = new Wall(scene,60,0.5*Math.PI,120,10)// wall left of carousel
+    const wall5 = new Wall(scene,40,0,70,0) //wall behind spinning cubes
+    const wall6 = new Wall(scene,80,0.5*Math.PI,30,-20) //wall left of player spawn
+    const wall7 = new Wall(scene,160,0.5*Math.PI,70,-90) // wall behind coaster
 }
 
 export {buildWalls};
